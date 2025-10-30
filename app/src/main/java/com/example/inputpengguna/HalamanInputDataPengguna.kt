221b3e5fc1cfa90a0e9631d3d0e073d3bed5b75e
nabilasaf <nabilasafitri861@gmail.com>
2025-10-30 09:16:29 +0700

    Menambahkan memperbaiki bagian by yang error

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -55,6 +57,8 @@ fun FormDataDiri(modifier: Modifier
             }
         }
         OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
 
         )
     }
