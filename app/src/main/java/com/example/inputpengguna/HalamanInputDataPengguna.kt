b27b5bbc8e23a8d42efea494b5341e061af2c506
nabilasaf <nabilasafitri861@gmail.com>
2025-10-30 08:53:16 +0700

    Menambahkan var nama,alamat, dan jenis kelamin

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -1,8 +1,21 @@
 package com.example.inputpengguna
 
+import android.R
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
-){}
+){
+    var textNama by remember { mutableStateOf(value = " ") }
+    var textAlamat by remember { mutableStateOf(value = " ") }
+    var textJK by remember { mutableStateOf(value = " ") }
+
+    var nama by remember { mutableStateOf(value = " ") }
+    var alamat by remember { mutableStateOf(value = " ") }
+    var jenis by remember { mutableStateOf(value = " ") }
+
+
+}
