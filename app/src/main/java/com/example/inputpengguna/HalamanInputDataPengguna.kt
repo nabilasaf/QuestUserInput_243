a5d4ddcdaaff9c2bee6f4927a9f15bf73deeef41
nabilasaf <nabilasafitri861@gmail.com>
2025-10-30 09:03:55 +0700

    Menambahkan Row di dalam column

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -3,6 +3,7 @@ package com.example.inputpengguna
 import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -42,6 +43,9 @@ fun FormDataDiri(modifier: Modifier
                 textNama = it
             }
         )
+        Row {
+
+        }
 
     }
 }
