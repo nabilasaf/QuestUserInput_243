4dad9533bf51655eb11e1852ab287ea095de6b10
nabilasaf <nabilasafitri861@gmail.com>
2025-11-01 11:12:09 +0700

    Menambahkan var dan val status

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -34,12 +34,15 @@ fun FormDataDiri(modifier: Modifier
     var textNama by remember { mutableStateOf(value = " ") }
     var textAlamat by remember { mutableStateOf(value = " ") }
     var textJK by remember { mutableStateOf(value = " ") }
+    var textSP by remember {mutableStateOf(value = " ")  }
 
     var nama by remember { mutableStateOf(value = " ") }
     var alamat by remember { mutableStateOf(value = " ") }
     var jenis by remember { mutableStateOf(value = " ") }
+    var StatusPerkawinan by remember { mutableStateOf(value = " ") }
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
+    var status: List<String> = listOf("Janda","Lajang","Duda","Menikah")
 
     Column (modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
