d613fc184335cd7f60456b88db1a2d8a353204e1
nabilasaf <nabilasafitri861@gmail.com>
2025-11-01 11:24:26 +0700

    Menghapus code yang tidak di gunakan

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -56,13 +56,15 @@ fun FormDataDiri(modifier: Modifier
         .fillMaxWidth(),
         verticalArrangement = Arrangement.Center
 
-    ){
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.DarkGray)
-            .padding(top = 70.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.DarkGray)
+                .padding(top = 70.dp)
         ) {
-            Text(text = stringResource(id= R.string.pendaftaran),
+            Text(
+                text = stringResource(id = R.string.pendaftaran),
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
@@ -71,47 +73,59 @@ fun FormDataDiri(modifier: Modifier
             )
 
         }
-        Spacer(modifier= Modifier.height(height = 0.dp))
+        Spacer(modifier = Modifier.height(height = 0.dp))
 
-        Card (modifier = Modifier
-            .background(color = Color.LightGray)
-            .fillMaxWidth(fraction = 1f)
-            .fillMaxSize(fraction = 1f)
-            .padding(all = 20.dp),
+        Card(
+            modifier = Modifier
+                .background(color = Color.LightGray)
+                .fillMaxWidth(fraction = 1f)
+                .fillMaxSize(fraction = 1f)
+                .padding(all = 20.dp),
 
             colors = CardDefaults.cardColors(
-                containerColor = Color.White)){
-            Spacer(modifier= Modifier.height(height = 20.dp))
-            Text(text = stringResource(id = R.string.Nama),
+                containerColor = Color.White
+            )
+        ) {
+            Spacer(modifier = Modifier.height(height = 20.dp))
+            Text(
+                text = stringResource(id = R.string.Nama),
                 fontSize = 20.sp,
                 fontFamily = FontFamily.Serif,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
                 modifier = Modifier.padding(top = 10.dp)
-                    .padding(horizontal = 20.dp))
-            Spacer(modifier= Modifier.height(height = 10.dp))
+                    .padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(height = 10.dp))
             OutlinedTextField(
                 value = textNama,
                 singleLine = true,
                 modifier = Modifier.width(width = 400.dp)
                     .padding(horizontal = 10.dp),
-                leadingIcon = {Text(text = "Isi Nama Lengkap",
-                    modifier = Modifier.padding(start = 5.dp))},
+                leadingIcon = {
+                    Text(
+                        text = "Isi Nama Lengkap",
+                        modifier = Modifier.padding(start = 5.dp)
+                    )
+                },
                 onValueChange = {
                     textNama = it
                 })
-            Spacer(modifier= Modifier.height(height = 10.dp))
-            Text(text = stringResource(id = R.string.Jenis),
+            Spacer(modifier = Modifier.height(height = 10.dp))
+            Text(
+                text = stringResource(id = R.string.Jenis),
                 fontSize = 20.sp,
                 fontFamily = FontFamily.Serif,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
                 modifier = Modifier.padding(top = 10.dp)
-                    .padding(horizontal = 20.dp))
-            Spacer(modifier= Modifier.height(height = 10.dp))
+                    .padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(height = 10.dp))
             Column {
                 gender.forEach { item ->
-                    Row(modifier = Modifier.selectable(
+                    Row(
+                        modifier = Modifier.selectable(
                         selected = textJK == item,
                         onClick = { textJK = item }
                     ), verticalAlignment = Alignment.CenterVertically) {
@@ -125,18 +139,21 @@ fun FormDataDiri(modifier: Modifier
                     }
                 }
             }
-            Spacer(modifier= Modifier.height(height = 10.dp))
-            Text(text = stringResource(id = R.string.Status),
+            Spacer(modifier = Modifier.height(height = 10.dp))
+            Text(
+                text = stringResource(id = R.string.Status),
                 fontSize = 20.sp,
                 fontFamily = FontFamily.Serif,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
                 modifier = Modifier.padding(top = 10.dp)
-                    .padding(horizontal = 20.dp))
-            Spacer(modifier= Modifier.height(height = 10.dp))
-            Column  {
+                    .padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(height = 10.dp))
+            Column {
                 status.forEach { item ->
-                    Row(modifier = Modifier.selectable(
+                    Row(
+                        modifier = Modifier.selectable(
                         selected = textJK == item,
                         onClick = { textJK = item }
                     ), verticalAlignment = Alignment.CenterVertically) {
@@ -150,76 +167,44 @@ fun FormDataDiri(modifier: Modifier
                     }
                 }
             }
-            Spacer(modifier= Modifier.height(height = 1.dp))
-            Text(text = stringResource(id = R.string.Alamat),
+            Spacer(modifier = Modifier.height(height = 1.dp))
+            Text(
+                text = stringResource(id = R.string.Alamat),
                 fontSize = 20.sp,
                 fontFamily = FontFamily.Serif,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
                 modifier = Modifier.padding(top = 10.dp)
-                    .padding(horizontal = 20.dp))
-            Spacer(modifier= Modifier.height(height = 10.dp))
+                    .padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(height = 10.dp))
             OutlinedTextField(
                 value = textAlamat,
                 singleLine = true,
                 modifier = Modifier.width(width = 400.dp)
                     .padding(horizontal = 10.dp),
-                leadingIcon = {Text(text = "Alamat",
-                    modifier = Modifier.padding(start = 5.dp))},
+                leadingIcon = {
+                    Text(
+                        text = "Alamat",
+                        modifier = Modifier.padding(start = 5.dp)
+                    )
+                },
                 onValueChange = {
                     textAlamat = it
                 })
-            Spacer(modifier= Modifier.height(height = 10.dp))
-            Button(modifier = Modifier.padding(horizontal = 50.dp)
-                .width(width = 500.dp),
+            Spacer(modifier = Modifier.height(height = 10.dp))
+            Button(
+                modifier = Modifier.padding(horizontal = 50.dp)
+                    .width(width = 500.dp),
                 enabled = textAlamat.isNotEmpty(),
                 onClick = {
-                    nama=textNama
-                    jenis=textJK
-                    alamat=textAlamat
-                    status= listOf(textSP)
+                    nama = textNama
+                    jenis = textJK
+                    alamat = textAlamat
+                    status = listOf(textSP)
                 }
-            ){
+            ) {
                 Text(text = stringResource(id = R.string.Submit))
-            }
-        }
-        HorizontalDivider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(id = R.dimen.padding_medium),
-                top = dimensionResource(id = R.dimen.padding_medium)
-                ),
-            thickness = dimensionResource(id = R.dimen.divider_tipis),
-            color = Color.LightGray
-        )
-        Button(modifier = Modifier.fillMaxWidth(fraction = 1f),
-           enabled = textAlamat.isNotEmpty(),
-            onClick = {
-                nama=textNama
-                jenis=textJK
-                alamat=textAlamat
-            }
-        ){
-            Text(text = stringResource(id = R.string.Submit))
-        }
-        HorizontalDivider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(id = R.dimen.padding_medium),
-                top = dimensionResource(id = R.dimen.padding_medium)
-            ),
-            thickness = dimensionResource(R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
-        ElevatedCard (
-            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.DarkGray),
-            modifier = Modifier
-                .height (height = 100.dp)
-                .width(width = 300.dp)
-        ) {
-            Column (modifier = Modifier.padding(horizontal = 10.dp, vertical = 15.dp),) {
-                Text(text= "Nama : "+nama, color = Color.White)
-                Text(text= "Gender : "+jenis, color = Color.White)
-                Text(text= "Alamat : "+alamat, color = Color.White)
             }
         }
     }
