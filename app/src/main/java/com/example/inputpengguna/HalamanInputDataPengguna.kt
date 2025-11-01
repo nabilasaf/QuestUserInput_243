fbeaa7cbde03b3257c0376598dfcc41184f48eff
nabilasaf <nabilasafitri861@gmail.com>
2025-11-01 11:15:13 +0700

    Menambahkan Spacer

--- app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
+++ app/src/main/java/com/example/inputpengguna/HalamanInputDataPengguna.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -68,6 +69,8 @@ fun FormDataDiri(modifier: Modifier
             )
 
         }
+        Spacer(modifier= Modifier.height(height = 0.dp))
+
         OutlinedTextField(
             value = textAlamat,
             singleLine = true,
